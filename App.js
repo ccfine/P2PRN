1916d36b95ab54e9cef8fb060d5d68ee53226a06
ChenFeng <ccfine24@163.com>
2018-06-12 11:39:47 +0800

    基本项目架构

--- App.js
+++ App.js
@@ -1,58 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from "react"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { Provider } from "react-redux"
+import reducer from "./src/redux/reducer.js"
+import Navigator from "./src/navigation/navigation.js"
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const store = createStore(reducer, applyMiddleware(thunk))
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      hasError: false
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+  componentDidCatch (error, info) {
+    this.setState({
+      hasError: true
+    })
+  }
+
+  render () {
+    return this.state.hasError? null: (
+      <Provider store={ store }>
+        <Navigator />
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
